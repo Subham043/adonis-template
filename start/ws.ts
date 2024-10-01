950b4486ace82517556c9d851ef73bb2c7c11f4a
Subham043 <sahasubham043@gmail.com>
2024-10-01 19:43:23 +0530

    done

--- start/ws.ts
+++ start/ws.ts
@@ -1,14 +1,7 @@
 import app from '@adonisjs/core/services/app'
 import Ws from '#services/ws_service'
-import { HttpContext } from '@adonisjs/core/http'
 
 app.ready(() => {
     Ws.boot()
     Ws.connectSocket()
-})
-
-app.terminating(() => {
-  const ctx = HttpContext.get()
-  if (!ctx) return
-  ctx.socket.disconnect()
 })
